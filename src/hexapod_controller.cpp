5c5f62f5a24b71d2ba3d3defed78390f3af1f6ae
ghada <gbo013@post.uit.no>
2017-10-11 20:18:38 +0200

    put all methods in local simulate for conveniance

--- src/hexapod_controller.cpp
+++ src/hexapod_controller.cpp
@@ -208,8 +208,9 @@ void Hexapod_controller::localSimulate(double dt) {
         std::cout<<"Inverse Kinematics is run !"<<std::endl;
         IK = true;
     }
-
-    walk_backward(*tripod.get(),dt);
+    walk_forward(*tripod.get(),dt);
+//    walk_backward(*tripod.get(),dt);
+//    run(dt);
 
 
 }
